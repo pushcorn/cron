edf24e4ce846b210eb8a8d67698baddce793912d
Joseph Tzeng <joseph.tzeng@gmail.com>
2024-01-19 23:16:53 -0500

    adds the cron.Server class

---
+++ test/tests/lib/cron/Server.js
@@ -0,0 +1,220 @@
+test.method ("cron.Server", "start")
+    .useMockDatabase ({ suffix: ".start" })
+    .should ("start the cron server")
+        .mock ("object", "updateEnqueueTimer")
+        .mock (Date, "now", function ()
+        {
+            return this.strategy.time1.getTime ();
+        })
+        .up (s => s.createArgs = { db: s.db })
+        .spy ("db", "update", async function (table, values)
+        {
+            values.mtime = this.strategy.time1;
+        })
+        .spy ("db", "execute", async function (statement)
+        {
+            let { target: db, strategy: s } = this;
+
+            if (statement instanceof s.Query)
+            {
+                statement = statement.sql;
+            }
+
+            if (statement == nit.trim.text`
+                SELECT *
+                FROM "cron_jobs"`)
+            {
+                await db.insert ("cron_jobs",
+                {
+                    id: s.uuid1,
+                    expr: "0 0 * * *",
+                    command: "shell echo test",
+                    timezone: "UTC",
+                    status: "running",
+                    mtime: s.time1
+                });
+            }
+        })
+        .after (s => s.object.stop ())
+        .expectingPropertyToBe ("spies.0.invocations.0.args.1",
+        {
+            nextRun: "2024-01-18T00:00:00.000",
+            status: "scheduled",
+            mtime: new Date ("2024-01-17T20:30:00.000Z"),
+            nextRunUtc: new Date ("2024-01-18T00:00:00.000Z")
+        })
+        .commit ()
+;
+
+
+test.method ("cron.Server", "dequeue")
+    .should ("dequeue a job from the database")
+        .useMockDatabase ({ suffix: ".dequeue" })
+        .up (s => s.createArgs = { db: s.db })
+        .mock ("object", "updateEnqueueTimer")
+        .mock ("object", "runJob")
+        .mock ("object", "dequeue", function ()
+        {
+            let { iteration, target, targetMethod } = this;
+
+            if (iteration != 2)
+            {
+                return nit.invoke ([target, targetMethod], arguments);
+            }
+        })
+        .mock ("db", "insert", function (table, values)
+        {
+            let { target, targetMethod, strategy: s } = this;
+
+            values.mtime = s.time1;
+
+            return nit.invoke ([target, targetMethod], [table, values]);
+        })
+        .mock (Date, "now", function ()
+        {
+            return this.strategy.time1.getTime ();
+        })
+        .mock ("db", "disconnect")
+        .spy ("db", "update", async function (table, values)
+        {
+            values.mtime = this.strategy.time1;
+        })
+        .before (s => s.object.start ())
+        .before (s =>
+        {
+            s.db.rewrite ("SELECT UUID_GENERATE_V4 ()", "SELECT 'aa69a37c-811a-4537-b3da-88b7af70be1c' AS uuid_generate_v4");
+            s.db.rewrite ("COMMIT", "-- COMMIT");
+        })
+        .before (s => s.object.Job.create ("", "0 0 * * *", "shell echo 'test'", s.timezone1, { status: "queued" }))
+        .after (s => s.object.taskQueue.waitUntilIdle ())
+        .after (s => s.object.stop ())
+        .after (s => s.mocks[5].restore ())
+        .after (s => s.db.disconnect ())
+        .expectingPropertyToBe ("spies.0.invocations.length", 1)
+        .expectingPropertyToBe ("mocks.0.invocations.length", 1)
+        .expectingPropertyToBe ("mocks.1.invocations.length", 1)
+        .expectingPropertyToBe ("mocks.2.invocations.length", 2)
+        .commit ()
+;
+
+
+test.method ("cron.Server", "runJob")
+    .should ("save the output of the job")
+        .useMockDatabase ({ suffix: ".runJob" })
+        .up (s => s.createArgs = { db: s.db })
+        .mock (nit, "lookupCommand", function ()
+        {
+            return nit.defineCommand ("test.commands.MyCmd")
+                .onRun (function ()
+                {
+                    return "my-cmd";
+                })
+            ;
+        })
+        .mock ("db", "insert", function (table, values)
+        {
+            let { target, targetMethod, strategy: s } = this;
+
+            values.mtime = s.time1;
+
+            return nit.invoke ([target, targetMethod], [table, values]);
+        })
+        .mock ("db", "update", function (table, values)
+        {
+            let { target, targetMethod } = this;
+
+            values.duration = 200;
+
+            return nit.invoke ([target, targetMethod], [table, values]);
+        })
+        .mock ("object", "updateEnqueueTimer")
+        .mock ("db", "disconnect")
+        .mock ("TimestampUpdater.prototype", "perform")
+        .given ("aa69a37c-811a-4537-b3da-88b7af70be1c")
+        .before (s => s.object.start ())
+        .before (s =>
+        {
+            s.db.rewrite ("SELECT UUID_GENERATE_V4 ()", "SELECT 'aa69a37c-811a-4537-b3da-88b7af70be1c' AS uuid_generate_v4");
+        })
+        .before (s => s.object.Job.create ("", "0 0 * * *", "shell echo 'test'", s.timezone1, { status: "queued" }))
+        .after (s => s.object.taskQueue.waitUntilIdle ())
+        .after (s => s.object.stop ())
+        .after (s => s.mocks[4].restore ())
+        .after (s => s.db.disconnect ())
+        .expectingPropertyToContain ("mocks.2.invocations.0.args.1",
+        {
+            status: "scheduled",
+            output: "my-cmd"
+        })
+        .expectingPropertyToBe ("mocks.3.invocations.length", 2)
+        .commit ()
+;
+
+
+test.method ("cron.Server", "updateEnqueueTimer")
+    .should ("set up the timer for the next scheduled job")
+        .useMockDatabase ({ suffix: ".updateEnqueueTimer" })
+        .up (s => s.createArgs = { db: s.db })
+        .mock ("db", "insert", function (table, values)
+        {
+            let { target, targetMethod, strategy: s } = this;
+
+            values.mtime = s.time1;
+
+            return nit.invoke ([target, targetMethod], [table, values]);
+        })
+        .mock ("object", "enqueueScheduledJobs")
+        .mock ("class.Task.prototype", "sleep", () => true)
+        .mock ("db", "disconnect")
+        .mock ("TimestampUpdater.prototype", "perform")
+        .mock (Date, "now", function ()
+        {
+            return this.strategy.time1.getTime ();
+        })
+        .mock ("object", "info")
+        .given ("aa69a37c-811a-4537-b3da-88b7af70be1c")
+        .before (s => s.object.start ())
+        .before (s =>
+        {
+            s.db.rewrite ("SELECT UUID_GENERATE_V4 ()", "SELECT 'aa69a37c-811a-4537-b3da-88b7af70be1c' AS uuid_generate_v4");
+        })
+        .before (s => s.object.Job.create ("", "0 0 * * *", "shell echo 'test'", s.timezone1))
+        .after (s => s.object.taskQueue.waitUntilIdle ())
+        .after (s => s.object.stop ())
+        .after (s => s.mocks[3].restore ())
+        .after (s => s.db.disconnect ())
+        .expectingPropertyToBe ("mocks.1.invocations.length", 1)
+        .expectingPropertyToBe ("mocks.2.invocations.0.args.0", 30600000)
+        .expectingPropertyToBe ("mocks.6.invocations.0.args.0", "info.next_job_scheduled")
+        .commit ()
+;
+
+
+test.method ("cron.Server", "enqueueScheduledJobs")
+    .should ("update scheduled jobs to queued")
+        .useMockDatabase ({ suffix: ".enqueueScheduledJobs" })
+        .up (s => s.createArgs = { db: s.db })
+        .mock ("db", "insert", function (table, values)
+        {
+            let { target, targetMethod, strategy: s } = this;
+
+            values.mtime = s.time1;
+
+            return nit.invoke ([target, targetMethod], [table, values]);
+        })
+        .mock ("object", "dequeue")
+        .mock ("db", "disconnect")
+        .given ("aa69a37c-811a-4537-b3da-88b7af70be1c")
+        .before (s => s.object.start ())
+        .before (s =>
+        {
+            s.db.rewrite ("SELECT UUID_GENERATE_V4 ()", "SELECT 'aa69a37c-811a-4537-b3da-88b7af70be1c' AS uuid_generate_v4");
+        })
+        .before (s => s.object.Job.create ("", "0 0 * * *", "shell echo 'test'", s.timezone1))
+        .after (s => s.object.taskQueue.waitUntilIdle ())
+        .after (s => s.object.stop ())
+        .after (s => s.mocks[2].restore ())
+        .after (s => s.db.disconnect ())
+        .expectingPropertyToBe ("mocks.1.invocations.length", 1)
+        .commit ()
+;
---
+++ test/tests/lib/cron/models/Job.js
@@ -0,0 +1,67 @@
+test.object ("cron.models.Job", true, "timeUntilNextRun")
+    .should ("return the time to next run")
+        .up (s => s.now = s.cron.getDateAsUtc (new Date (2024, 1, 12, 17, 20)) * 1)
+        .up (s => s.args =
+        {
+            expr: "0 0 * * *",
+            command: "shell echo test",
+            timezone: "UTC",
+            nextRunUtc: s.now
+        })
+        .up (s => s.Date = Date)
+        .mock ("Date", "now", function ()
+        {
+            return this.strategy.now;
+        })
+        .before (s => s.class.validate (s.instance))
+        .returns (24000000)
+        .expectingPropertyToBe ("instance.timeUntilNextRunHumanized", "6 hours and 40 minutes")
+        .expectingPropertyToBeOfType ("class.table", "postgresql.Table")
+        .expectingPropertyToBe ("class.table.indexes.length", 1)
+        .commit ()
+;
+
+
+test.method ("cron.models.Job", "update")
+    .should ("update the nextRun fields")
+        .useMockPgClient ()
+        .up (s => s.now = s.cron.getDateAsUtc (new Date (2024, 1, 12, 17, 20)) * 1)
+        .up (s => s.createArgs =
+        {
+            id: "11111111-1111-1111-1111-111111111111",
+            expr: "0 0 * * *",
+            command: "shell echo test",
+            timezone: "UTC",
+            nextRunUtc: s.now
+        })
+        .up (s => s.Date = Date)
+        .mock ("Date", "now", function ()
+        {
+            return this.strategy.now;
+        })
+        .spy ("db", "update", function (table, changes)
+        {
+            changes.mtime = new Date (this.strategy.now);
+        })
+        .before (s => s.db.client.result =
+        {
+            rows:
+            [
+            {
+                id: "11111111-1111-1111-1111-111111111111",
+                expr: "0 0 * * *",
+                command: "shell echo test",
+                timezone: "UTC"
+            }
+            ]
+        })
+        .before (s => s.class.db = s.db)
+        .given ({ status: "running" })
+        .returnsResultOfExpr ("object")
+        .expectingPropertyToBe ("db.client.statement", nit.trim.text`
+            UPDATE "cron_jobs"
+            SET "nextRun" = '2024-02-13T00:00:00.000', "status" = 'running', "mtime" = '2024-02-12T17:20:00.000Z', "nextRunUtc" = '2024-02-13T00:00:00.000Z'
+            WHERE "id" = '11111111-1111-1111-1111-111111111111'
+        `)
+        .commit ()
+;
