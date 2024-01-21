8a6521c41fff79fcdd9bbac1dd2de60c9e0f9bd6
Joseph Tzeng <joseph.tzeng@gmail.com>
2024-01-21 15:42:20 -0500

    adds cron.apis.UpdateJob

--- test/tests/lib/cron/apis/UpdateJob.js
+++ test/tests/lib/cron/apis/UpdateJob.js
@@ -1,4 +1,4 @@
-test.api ("cron.apis.RemoveJob")
+test.api ("cron.apis.UpdateJob")
     .setupTestForApi ()
     .should ("return JobNotFound if the ID is invalid UUID")
         .up (s => s.endpoint = "GET cron/jobs/1234")
@@ -12,33 +12,35 @@ test.api ("cron.apis.RemoveJob")
         .expectingPropertyToBeOfType ("context.response", "cron.responses.JobNotFound")
         .commit ()
 
-    .should ("remove a scheduled job")
-        .up (s => s.endpoint = "DELETE cron/jobs/" + s.uuid1)
+    .should ("update a scheduled job")
+        .up (s => s.endpoint = "PUT cron/jobs/" + s.uuid1)
         .mock (Date, "now", function () { return this.strategy.time1 * 1; })
         .spy ("db", "insert", async function (table, values) { values.mtime = this.strategy.time1; })
+        .spy ("db", "update", async function (table, values) { values.mtime = this.strategy.time1; })
         .before (s =>
         {
             s.db.rewrite ("SELECT UUID_GENERATE_V4 ()", `SELECT '${s.uuid1}' AS uuid_generate_v4`);
             s.db.rewrite ("COMMIT", "-- COMMIT");
         })
         .before (s => s.object.on ("preDispatch", (api, ctx) => ctx.Job.create ("", "0 0 * * *", "nit test", "America/Indianapolis")))
-        .expectingPropertyToBeOfType ("context.response", "cron.responses.JobRemoved")
+        .given ({ data: { command: "shell nit test", expr: "0 1 * * *" } })
+        .expectingPropertyToBeOfType ("context.response", "cron.responses.JobUpdated")
         .expectingMethodToReturnValue ("context.response.toPojo", null,
         {
             "job":
             {
-                "command": "nit test",
+                "command": "shell nit test",
                 "duration": 0,
                 "error": "",
                 "exitCode": 0,
-                "expr": "0 0 * * *",
+                "expr": "0 1 * * *",
                 "id": "11111111-1111-1111-1111-111111111111",
-                "nextRun": "2024-01-18T00:00:00.000-05:00",
+                "nextRun": "2024-01-18T01:00:00.000-05:00",
                 "nextRunUtc": new Date ("2023-09-03T04:00:00.000Z"),
                 "output": "",
                 "status": "scheduled",
-                "timeUntilNextRun": 30600000,
-                "timeUntilNextRunHumanized": "8 hours and 30 minutes",
+                "timeUntilNextRun": 34200000,
+                "timeUntilNextRunHumanized": "9 hours and 30 minutes",
                 "timezone": "America/Indianapolis"
             }
         })
