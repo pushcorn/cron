018bedcb41b8c0a37ca6b2755ac5ab2bd737be1c
Joseph Tzeng <joseph.tzeng@gmail.com>
2024-01-21 14:57:00 -0500

    adds cron.apis.RemoveJob

--- test/tests/lib/cron/apis/RemoveJob.js
+++ test/tests/lib/cron/apis/RemoveJob.js
@@ -1,47 +1,44 @@
 test.api ("cron.apis.RemoveJob")
-    .should ("return error if the job ID is less than 1.")
-        .expectingPropertyToBeOfType ("context.response", "http.responses.ValidationFailed")
+    .setupTestForApi ()
+    .should ("return JobNotFound if the ID is invalid UUID")
+        .up (s => s.endpoint = "GET cron/jobs/1234")
+        .before (s => s.mock (nit.log, "e"))
+        .expectingPropertyToBeOfType ("context.response", "cron.responses.JobNotFound")
+        .expectingPropertyToBe ("mocks.0.invocations.length", 1)
         .commit ()
 
-    .should ("return error if the job was not found")
-        .given ({}, { pathParams: { id: 1 } })
+    .should ("return JobNotFound if the ID is not found")
+        .up (s => s.endpoint = "GET cron/jobs/" + s.uuid1)
         .expectingPropertyToBeOfType ("context.response", "cron.responses.JobNotFound")
         .commit ()
 
     .should ("remove a schedule job")
-        .given ({}, { pathParams: { id: 1 } })
-        .up (s => s.global = global)
-        .mock ("global.Date", "now", function ()
+        .up (s => s.endpoint = "DELETE cron/jobs/" + s.uuid1)
+        .mock (Date, "now", function () { return this.strategy.time1 * 1; })
+        .spy ("db", "insert", async function (table, values) { values.mtime = this.strategy.time1; })
+        .before (s =>
         {
-            let { iteration, strategy: s } = this;
-
-            if (iteration == 1)
-            {
-                s.now = nit.Date ("2023-09-02 14:55", "America/Indianapolis").getTime ();
-            }
-
-            return s.now;
+            s.db.rewrite ("SELECT UUID_GENERATE_V4 ()", `SELECT '${s.uuid1}' AS uuid_generate_v4`);
+            s.db.rewrite ("COMMIT", "-- COMMIT");
         })
-        .before (s => s.Scheduler.get (s.context.server).schedule (nit.new ("cron.Job",
-        {
-            expr: "0 0 * * *",
-            command: "nit test",
-            timezone: "America/Indianapolis"
-        })))
+        .before (s => s.object.on ("preDispatch", (api, ctx) => ctx.Job.create ("", "0 0 * * *", "nit test", "America/Indianapolis")))
         .expectingPropertyToBeOfType ("context.response", "cron.responses.JobRemoved")
         .expectingMethodToReturnValue ("context.response.toPojo", null,
         {
             "job":
             {
                 "command": "nit test",
+                "duration": 0,
+                "error": "",
+                "exitCode": 0,
                 "expr": "0 0 * * *",
-                "id": 1,
-                "lastExitCode": -1,
-                "nextRun": "2023-09-03T00:00:00.000-04:00",
-                "nextRunUtc": "2023-09-03T04:00:00.000Z",
-                "shell": true,
-                "timeUntilNextRun": 32700000,
-                "timeUntilNextRunHumanized": "9 hours and 5 minutes",
+                "id": "11111111-1111-1111-1111-111111111111",
+                "nextRun": "2024-01-18T00:00:00.000-05:00",
+                "nextRunUtc": new Date ("2023-09-03T04:00:00.000Z"),
+                "output": "",
+                "status": "scheduled",
+                "timeUntilNextRun": 30600000,
+                "timeUntilNextRunHumanized": "8 hours and 30 minutes",
                 "timezone": "America/Indianapolis"
             }
         })
