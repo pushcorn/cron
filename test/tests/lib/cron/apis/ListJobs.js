1b51d0a820948e58062c034c8936f18712e06663
Joseph Tzeng <joseph.tzeng@gmail.com>
2024-01-21 14:52:02 -0500

    adds cron.apis.ListJobs

--- test/tests/lib/cron/apis/ListJobs.js
+++ test/tests/lib/cron/apis/ListJobs.js
@@ -1,28 +1,27 @@
 test.api ("cron.apis.ListJobs")
+    .setupTestForApi ()
     .should ("list scheduled jobs")
-    .up (s => s.Date = Date)
-    .mock ("Date", "now", nit.Date ("2023-09-18 15:35", "America/Indianapolis").getTime ())
-    .before (s => s.scheduler = s.Scheduler.get (s.context.server))
-    .before (s => s.scheduler.jobMap[9] = nit.new ("cron.Job", 9, "0 0 * * *", "nit test", "America/Indianapolis"))
-    .before (s => s.scheduler.jobMap[10] = nit.new ("cron.Job", 1, "0 0 * * *", "nit test", "America/Indianapolis"))
-    .before (s => s.scheduler.jobMap[9].next (nit.Date ("2023-09-18 15:00", "America/Indianapolis")))
-    .before (s => s.scheduler.jobMap[10].next (nit.Date ("2023-09-18 15:00", "America/Indianapolis")))
-    .expectingPropertyToBeOfType ("context.response", "cron.responses.JobListReturned")
-    .expectingPropertyToBe ("context.response.jobs.length", 2)
-    .expectingPropertyToBe ("context.response.jobs.0.id", 9)
-    .expectingPropertyToBe ("context.response.jobs.1.id", 10)
-    .expectingMethodToReturnValue ("context.response.jobs.0.toPojo", null,
-    {
-        "command": "nit test",
-        "expr": "0 0 * * *",
-        "id": 9,
-        "lastExitCode": -1,
-        "nextRun": "2023-09-19T00:00:00.000-04:00",
-        "nextRunUtc": "2023-09-19T04:00:00.000Z",
-        "shell": true,
-        "timeUntilNextRun": 30300000,
-        "timeUntilNextRunHumanized": "8 hours and 25 minutes",
-        "timezone": "America/Indianapolis"
-    })
-    .commit ()
+        .mock (Date, "now", function () { return this.strategy.time1 * 1; })
+        .spy ("db", "insert", async function (table, values) { values.mtime = this.strategy.time1; })
+        .before (s => s.db.rewrite ("SELECT UUID_GENERATE_V4 ()", `SELECT '${s.uuid1}' AS uuid_generate_v4`))
+        .before (s => s.object.on ("preDispatch", (api, ctx) => ctx.Job.create ("", "0 0 * * *", "nit test", "America/Indianapolis")))
+        .expectingPropertyToBeOfType ("context.response", "cron.responses.JobsReturned")
+        .expectingPropertyToBe ("context.response.jobs.length", 1)
+        .expectingMethodToReturnValue ("context.response.jobs.0.toPojo", null,
+        {
+            "command": "nit test",
+            "duration": 0,
+            "error": "",
+            "exitCode": 0,
+            "expr": "0 0 * * *",
+            "id": "11111111-1111-1111-1111-111111111111",
+            "nextRun": "2024-01-18T00:00:00.000-05:00",
+            "nextRunUtc": new Date ("2023-09-03T04:00:00.000Z"),
+            "output": "",
+            "status": "scheduled",
+            "timeUntilNextRun": 30600000,
+            "timeUntilNextRunHumanized": "8 hours and 30 minutes",
+            "timezone": "America/Indianapolis"
+        })
+        .commit ()
 ;
